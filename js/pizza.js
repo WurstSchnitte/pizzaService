b1573aee68ceb176fae9bccf659be31527a5baba
Mattheus van Iterson <sleepymaat@gmx.de>
2016-11-25 22:02:12 +0100

    menuitems

--- js/pizza.js
+++ js/pizza.js
@@ -23,6 +23,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function(){
   if(this.readyState == 4 && this.status == 200){
     document.getElementById("error").innerHtml = this.responseText;
+    menuitems = JSON.parse(this.responseText);
   }
 };
 xhr.open("GET", "../php/pizzen.php", true);
