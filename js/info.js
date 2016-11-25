8cc66687c3eb19def19ae360c3230993cfc55826
Mattheus van Iterson <sleepymaat@gmx.de>
2016-11-26 00:16:18 +0100

    wtf

--- js/info.js
+++ js/info.js
@@ -8,6 +8,6 @@ var init = function(){
       console.log(this.responseText);
     }
   };
-  xhr.open("GET", "../php/info.php", true);
-  xhr.send("id="+id);
+  xhr.open("GET", "../php/info.php?id="+id, true);
+  xhr.send();
 }
