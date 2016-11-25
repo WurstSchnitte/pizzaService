667a92b8dc9e5aea7443878f684622134d6ad60b
WurstSchnitte <PatriceDeluxe@web.de>
2016-11-25 21:32:23 +0100

    neu

--- php/bestellung.php
+++ php/bestellung.php
@@ -1,6 +1,10 @@
 <?php
-  $data = file_get_contents("php://input");// = json_decode();
-  echo var_dump($data);
-  
+  $data = file_get_contents("php://input", 0, null, null);// = json_decode();
+  $json = json_decode($data);
+  $bestellung = $json->bestellung;
+  for($i = 0; $i < sizeof($bestellung); $i++){
+    
+  }
+
   //echo var_dump($data);
 ?>
