019e97c78ea1a6c753bf5b83d4e5f55f787e11f7
Mattheus van Iterson <sleepymaat@gmx.de>
2016-11-25 22:08:31 +0100

    if

--- js/pizza.js
+++ js/pizza.js
@@ -22,7 +22,7 @@ var sum = 0;
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function(){
   if(this.readyState == 4 && this.status == 200){
-    console.log(JSON.parse(this.responseText));
+    console.log(JSON.parse(this.responseText).menuitems);
     menuitems = JSON.parse(this.responseText).menuitems;
   }
 };
