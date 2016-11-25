d6462413da8efa25da171d7b6aab74f95cd89468
WurstSchnitte <PatriceDeluxe@web.de>
2016-11-25 23:50:37 +0100

    message

--- php/order.php
+++ php/order.php
@@ -1,6 +1,39 @@
 <?php
-  $data = file_get_contents("php://input");// = json_decode();
-  echo var_dump($data);
-  
-  //echo var_dump($data);
+  $data = file_get_contents("php://input", 0, null, null);// = json_decode();
+  $json = json_decode($data);
+  $bestellung = $json->bestellung;
+  $servername = "localhost";
+  $username = "pizzaService";
+  $password = "wert";
+  $dbname = "pizzaService";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  $sql = "INSERT INTO `bestellung` (`id`, `adresse`, `preis`) VALUES (NULL, '$json->adresse', 0)";
+  if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $sql = "SELECT id FROM bestellung WHERE bestellung.adresse = '$json->adresse'";
+  $result = $conn->query($sql);
+
+  $bestellungsid = $result->fetch_assoc();
+
+  for($i = 0; $i < sizeof($bestellung); $i++){
+     $sql = "INSERT INTO `pizzabestellung` (`pizza_id`, `bestellung_id`, 'zustand') VALUES ('$bestellung[$i]', '$bestellungsid[id]', 0)";
+     if ($conn->query($sql) === TRUE) {
+         echo "New record created successfully";
+     } else {
+         echo "Error: " . $sql . "<br>" . $conn->error;
+     }
+  }
+  $conn->close();
+
+header("Location: ../html/info.html");
 ?>
